59558def12567d8f0e4c46432d3b1502fc6152db
satyam147 <satyam161211149@gmail.com>
2025-10-04 14:13:09 +0530

    add methods to Zoom interface for user, meeting, webinar, recording, and role

--- src/Contracts/Zoom.php
+++ src/Contracts/Zoom.php
@@ -3,6 +3,36 @@
 namespace MacsiDigital\Zoom\Contracts;
 
 
+use MacsiDigital\Zoom\Meeting;
+use MacsiDigital\Zoom\Recording;
+use MacsiDigital\Zoom\Role;
+use MacsiDigital\Zoom\User;
+use MacsiDigital\Zoom\Webinar;
+
 interface Zoom
 {
+    /**
+     * @return User
+     */
+    public function user(): User;
+
+    /**
+     * @return Meeting
+     */
+    public function meeting(): Meeting;
+
+    /**
+     * @return Webinar
+     */
+    public function webinar(): Webinar;
+
+    /**
+     * @return Recording
+     */
+    public function recording(): Recording;
+
+    /**
+     * @return Role
+     */
+    public function role(): Role;
 }
